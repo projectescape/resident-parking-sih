06a1dd3083d3aa4aaa11da0cb490b4efb042534e
projectEscape <escaperealityproject@gmail.com>
2020-01-25 03:55:02 +0530

    Initialized mongoose and setup in/out routes

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,12 +1,4 @@
 module.exports = {
-  client: "pg",
-  connection: {
-    host: "ec2-107-20-185-16.compute-1.amazonaws.com",
-    user: "mdtokvylcdygpk",
-    password:
-      "02b58f9917e5f8cee09dd823e300cb727c007c6209318472b2058e415f29ac55",
-    database: "d2b5m9ooffrvf5",
-    ssl: "true",
-    charset: "utf8"
-  }
+  uri:
+    "mongodb+srv://baniket99:baniket99@cluster0-i8j8v.mongodb.net/test?retryWrites=true&w=majority"
 };
--- server/index.js
+++ server/index.js
@@ -3,28 +3,126 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const dbConfig = require("./config/dbConfig");
-const bookshelf = require("bookshelf")(require("knex")(dbConfig));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(dbConfig.uri);
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 // Initializing models
 
-const InOut = bookshelf.Model.extend({
-  tableName: "in_out",
-  requireFetch: false
+const residentSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  phone: Number,
+  vehicle: String,
+  guest: [{ type: mongoose.Types.ObjectId, ref: "Guest" }],
+  inout: [{ type: mongoose.Types.ObjectId, ref: "InOut" }]
 });
-const Guest = bookshelf.Model.extend({
-  tableName: "guest",
-  requireFetch: false
+
+const Resident = mongoose.model("Resident", residentSchema);
+
+// Resident.create(
+//   {
+//     name: "Aniket",
+//     address: "D415",
+//     phone: 8708257683,
+//     vehicle: "HR10AE1090"
+//   },
+//   (e, newRes) => {
+//     console.log(newRes);
+//   }
+// );
+
+const guestSchema = new mongoose.Schema({
+  name: String,
+  phone: Number,
+  vehicle: String,
+  resident_id: { type: mongoose.Types.ObjectId, ref: "Resident" }
 });
-const UnAuth = bookshelf.Model.extend({
-  tableName: "unauth",
-  requireFetch: false
+
+const Guest = mongoose.model("Guest", guestSchema);
+
+const unAuthSchema = new mongoose.Schema({
+  name: String,
+  vehicle: String,
+  resident_add: { type: mongoose.Types.ObjectId, ref: "Resident" },
+  time: {
+    type: Date,
+    default: Date.now
+  }
 });
-const Resident = bookshelf.Model.extend({
-  tableName: "user",
-  requireFetch: false
+
+const UnAuth = mongoose.model("UnAuth", unAuthSchema);
+
+const inOutSchema = new mongoose.Schema({
+  vehicle: String,
+  guest_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "Guest",
+    default: null
+  },
+  resident_id: {
+    type: mongoose.Types.ObjectId,
+    ref: "Resident",
+    default: null
+  },
+  in_time: { type: Date, default: Date.now },
+  out_time: { type: Date, default: null }
+});
+
+const InOut = mongoose.model("InOut", inOutSchema);
+
+app.get("/test", (req, res) => {
+  res.send("Connected");
+});
+app.post("/security/in", async (req, res) => {
+  const { vehicle } = req.body;
+
+  const resident = await Resident.findOne({ vehicle });
+
+  //! Check if resident
+  if (resident) {
+    const inOut = await InOut.create({
+      resident_id: resident._id,
+      vehicle
+    });
+    io.emit("inData", inOut);
+  } else {
+    //! Check if guest
+
+    const guest = await Guest.findOne({ vehicle });
+    if (guest) {
+      const inOut = await InOut.create({
+        guest_id: guest._id,
+        vehicle
+      });
+      io.emit("inData", inOut);
+    } else {
+      //TODO Handle resident
+
+      console.log("Handle resident notification");
+    }
+  }
+
+  res.send("Succesfull");
+});
+app.post("/security/out", async (req, res) => {
+  const { vehicle } = req.body;
+
+  //! Upadate Out time
+  const inOut = await InOut.updateOne(
+    { vehicle, out_time: null },
+    { out_time: Date.now() }
+  );
+  console.log(inOut);
+  res.send("Succesfull");
 });
 
 // Connecting a user through socket
